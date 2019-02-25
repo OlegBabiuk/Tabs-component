ab32d7d8f89a9c9598f3a79d72afe513c97e24b1
OlegBabiuk <oleg21871@gmail.com>
2019-02-25 21:08:32 +0200

    Initial commit

---
+++ src/js/Tab.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function Tab() {
+  return (<p>Hello World</p>);
+}
+
+// Tab.PropTypes = {};
+// Tab.defaultProps = {};
+
+export default Tab;
---
+++ src/js/TabContainer.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable no-unused-vars */
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+// import PropTypes from 'prop-types';
+
+function TabContainer() {
+  return (<p />);
+}
+
+// Tab.PropTypes = {};
+// Tab.defaultProps = {};
+
+export default TabContainer;
